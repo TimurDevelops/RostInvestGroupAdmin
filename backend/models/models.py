102b8c1505f0b52d1890e0db4e9fe75ce9238743
TimurDevelops <Snakeemur@gmail.com>
2024-02-23 19:01:33 +0300

    Added tables

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,12 +1,25 @@
 import datetime
 
-from sqlalchemy import Integer, String, LargeBinary, Boolean, DateTime
+from sqlalchemy import Integer, String, LargeBinary, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.sql import func
 
 
 class Base(DeclarativeBase):
     pass
+
+    @classmethod
+    def as_dict(cls, user):
+        d_user = {}
+        for column in cls.__table__.columns:
+            if getattr(user, column.key, None):
+                if isinstance(user.__getattribute__(column.key), datetime.datetime):
+                    date = user.__getattribute__(column.key)
+                    d_user[column.key] = datetime.datetime.strftime(date, "%Y/%m/%d %H:%M:%S")
+                else:
+                    d_user[column.key] = user.__getattribute__(column.key)
+
+        return d_user
 
 
 class User(Base):
@@ -26,15 +39,36 @@ class User(Base):
         onupdate=func.current_timestamp()
     )
 
-    @classmethod
-    def as_dict(cls, user):
-        d_user = {}
-        for column in cls.__table__.columns:
-            if getattr(user, column.key, None):
-                if isinstance(user.__getattribute__(column.key), datetime.datetime):
-                    date = user.__getattribute__(column.key)
-                    d_user[column.key] = datetime.datetime.strftime(date, "%Y/%m/%d %H:%M:%S")
-                else:
-                    d_user[column.key] = user.__getattribute__(column.key)
 
-        return d_user
+class Category(Base):
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String, nullable=True)
+    parent_category_id: Mapped[int] = mapped_column(Integer, ForeignKey("category"))
+    category_image: Mapped[str] = mapped_column(String, nullable=True)
+    create_date: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, default=func.now())
+    update_date: Mapped[datetime.datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=func.now(),
+        onupdate=func.current_timestamp()
+    )
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String, nullable=True)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    parent_category_id: Mapped[int] = mapped_column(Integer, ForeignKey("category"))
+    product_image: Mapped[str] = mapped_column(String, nullable=True)
+    product_price: Mapped[str] = mapped_column(String, nullable=True)
+    create_date: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, default=func.now())
+    update_date: Mapped[datetime.datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=func.now(),
+        onupdate=func.current_timestamp()
+    )
